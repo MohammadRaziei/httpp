b7c520b8b83a098f04d51d38a8be5fd286b627f5
mohammadraziei <mohammadraziei1375@gmail.com>
2026-09-16 02:41:14 +0330

    Release 0.5.1

--- include/httpp.h
+++ include/httpp.h
@@ -39,7 +39,7 @@
 
 #define HTTPP_VERSION_MAJOR 0
 #define HTTPP_VERSION_MINOR 5
-#define HTTPP_VERSION_PATCH 0
+#define HTTPP_VERSION_PATCH 1
 
 /*==============================================================================
  * MARK: - Public API
